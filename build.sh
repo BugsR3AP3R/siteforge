564fb15415bd5287837cda1726fede62c9730254
BugsR3AP3R <arbensvital2@gmail.com>
2026-03-19 09:53:57 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-# Script de build exécuté par Render à chaque déploiement
-set -o errexit   # Arrêter si une commande échoue
+set -o errexit
 
 echo "==> Installation des dépendances..."
 pip install -r requirements.txt
@@ -10,5 +9,18 @@ python manage.py collectstatic --noinput
 
 echo "==> Application des migrations..."
 python manage.py migrate
+
+echo "==> Création du superuser (si inexistant)..."
+python manage.py shell -c "
+from accounts.models import User
+import os
+email = os.environ.get('DJANGO_SUPERUSER_EMAIL', '')
+password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
+if email and password and not User.objects.filter(email=email).exists():
+    User.objects.create_superuser(username=email, email=email, password=password)
+    print('Superuser créé :', email)
+else:
+    print('Superuser déjà existant ou variables manquantes.')
+"
 
 echo "==> Build terminé ✓"
